6291aa47179d1ebc1af6e3c16d212a828a6a7cae
shradhaN <shradha.neupane@yahoo.com>
2020-12-24 20:47:00 +0545

    Fix the failing test for vendor serializer hash

--- spec/serializers/spree/v2/storefront/vendor_serializer_spec.rb
+++ spec/serializers/spree/v2/storefront/vendor_serializer_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe Spree::V2::Storefront::VendorSerializer do
-  let(:vendor) { create(:vendor, name: 'Test', about_us: 'Hello World') }
+  let(:vendor) { create(:vendor, name: 'Test', about_us: 'Hello World', contact_us: "Contact Us", slug: "slug") }
 
   subject { described_class.new(vendor) }
 
@@ -11,10 +11,20 @@ describe Spree::V2::Storefront::VendorSerializer do
       {
         data: {
           id: vendor.id.to_s,
-          type: :vendor,
+          relationships: {
+            image: {
+              data: nil,
+              products: {
+                data: []
+              }
+            },
+            type: vendor
+          },
           attributes: {
             name: 'Test',
-            about_us: 'Hello World'
+            contact_us: "Contact Us",
+            about_us: 'Hello World',
+            slug: "slug"
           }
         }
       }
