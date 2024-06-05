b671e1f52bd5c60c646cc0cee7d7d223b62a40b8
Mausam Pun <mausam@truemark.tech>
2024-06-05 09:26:17 +0545

    API for vendor creation from storefront

--- app/controllers/spree/api/v2/storefront/vendors_controller.rb
+++ app/controllers/spree/api/v2/storefront/vendors_controller.rb
@@ -3,7 +3,36 @@ module Spree
     module V2
       module Storefront
         class VendorsController < ::Spree::Api::V2::ResourceController
-          
+          before_action :require_spree_current_user, only: %i[create update]
+          before_action :load_vendor, only: %i[update destroy]
+
+          def create
+            vendor = Spree::Vendor.new(vendor_params)
+            vendor.state = 'pending'
+            if vendor.save
+              create_vendor_user(vendor)
+              render_serialized_payload { serialize_resource(vendor) }
+            else
+              render_error_payload(vendor.errors)
+            end
+          end
+
+          def update
+            if @vendor.users.include?(spree_current_user) && @vendor.update(vendor_params)
+              render_serialized_payload { serialize_resource(@vendor) }
+            else
+              render_error_payload(@vendor.errors)
+            end
+          end
+
+          def destroy
+            if @vendor.users.include?(spree_current_user) && @vendor.destroy
+              render_serialized_payload { serialize_resource(@vendor) }
+            else
+              render_error_payload(@vendor.errors)
+            end
+          end
+
           private
 
           def model_class
@@ -26,6 +55,17 @@ module Spree
             Spree::V2::Storefront::VendorSerializer
           end
 
+          def vendor_params
+            params.require(:vendor).permit(Spree::PermittedAttributes.vendor_attributes)
+          end
+
+          def load_vendor
+            @vendor = Spree::Vendor.find(params[:id])
+          end
+
+          def create_vendor_user(vendor)
+            Spree::VendorUser.create(vendor_id: vendor.id, user_id: spree_current_user.id)
+          end
         end
       end
     end
